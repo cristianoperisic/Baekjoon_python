d62b73853a8f5fb94928a48873212f8efc5beae4
cristianoperisic <loststars0522@gmail.com>
2026-01-09 23:42:27 +0900

    14501modify

--- 14501.py
+++ 14501.py
@@ -1,3 +1,4 @@
+# 14501 퇴사 풀이 코드
 import sys
 
 input = sys.stdin.readline
@@ -9,6 +10,7 @@ ScoreArray = [0] * (N + 1)
 
 for i in range(1, N + 1):
     T[i], P[i] = map(int, input().split())
+# 여기까진 파이썬 입력 받는 걸 gpt를 이용해서 배움.
 
 if T[N] == 1:
     ScoreArray[N] = P[N]
@@ -24,3 +26,5 @@ for n in range(N - 1, 0, -1):
         ScoreArray[n] = max(ScoreArray[n + 1], ScoreArray[n + T[n]] + P[n])
 
 print(max(ScoreArray))
+
+# 더 좋은 방법은 애초에 ScoreArray의 크기를 N+2로 잡았다면 ScoreArray[N]초기화 필요없고 for 루프 안도 N부터 시작해도 됐었음
